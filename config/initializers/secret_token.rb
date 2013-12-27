fd7e43a2e9262564112aa6ad5193f4d435b5f0da
Rich Buggy <rich@buggy.id.au>
2013-12-27 22:54:21 +1100

    New Rails 4 application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ecommercepanel::Application.config.secret_key_base = '73a1ab437f13e63f6b214e944f52d5e4ab2b27cb0887f657a178bce947d6dc742e765225228eb9bb3a692eb4658e84e384f8efa681cda8c5cf468b6ca434e3dc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ecommercepanel::Application.config.session_store :cookie_store, key: '_ecommercepanel_session'
